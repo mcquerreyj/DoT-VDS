460c22e2d66fc0a851138362507addbd022e25d7
Allen Whitt <allen.whitt@gmail.com>
2014-05-28 18:18:04 -0400

    C# stuff

---
+++ NASAproj/NASAproj/Class1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NASAproj
+{
+    // StopEvents are objects that contain the necessary data from a stop event.
+    class StopEvent
+    {
+        int timestamp;
+        int eventcodeid;
+        int param;
+
+        // Constructors
+        public StopEvent(string stpevent)
+        {
+            char[] del = { ' ', ',', '.', ':', '\t' };
+            string[] stop = stpevent.Split(del);
+
+            setTime(Convert.ToInt32(stop[1]), Convert.ToInt32(stop[2]), Convert.ToInt32(stop[3]));
+            eventcodeid = Convert.ToInt32(stop[4]);
+            param = Convert.ToInt32(stop[5]);
+        }
+        public StopEvent(string time, string eventC, string par)
+        {
+            setTime(Convert.ToInt32(time.Substring(0, 2)), Convert.ToInt32(time.Substring(3, 2)), Convert.ToInt32(time.Substring(6)));
+            eventcodeid = Convert.ToInt32(eventC);
+            param = Convert.ToInt32(par);
+        }
+
+
+        // format 01:07.8
+        public void setTime(int hour, int min, int mili)
+        {
+            timestamp = hour *1000 + min *10  + mili;
+        }
+
+        int getTime()
+        {
+            return timestamp;
+        }
+
+        public string toString()
+        {
+            return timestamp.ToString() + " " + eventcodeid.ToString() + " " + param.ToString();
+        }
+    }
+
+    /*
+    class StopTest
+    {
+        static void Main(string[] args)
+        {
+            string evt = "327707,01:21.5,82,20";
+            StopEvent evnt1 = new StopEvent(evt);
+            Console.WriteLine(evnt1.toString());
+
+            string time = "01:21.5";
+            string code = "82";
+            string param = "20";
+            StopEvent evnt2 = new StopEvent(time, code, param);
+            Console.WriteLine(evnt2.toString());
+        }
+    }*/
+}
---
+++ NASAproj/NASAproj/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NASAproj
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

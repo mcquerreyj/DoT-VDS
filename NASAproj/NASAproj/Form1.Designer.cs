428e84e8b2fbc36a8fafb4ff13fd2cfa0944e442
mcquerreyj <mcquerrey@marshall.edu>
2014-05-29 17:51:43 -0400

    Pls no Overwriterino

--- NASAproj/NASAproj/Form1.Designer.cs
+++ NASAproj/NASAproj/Form1.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.makeButton = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
@@ -46,14 +46,14 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // makeButton
             // 
-            this.button1.Location = new System.Drawing.Point(534, 46);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.makeButton.Location = new System.Drawing.Point(534, 46);
+            this.makeButton.Name = "makeButton";
+            this.makeButton.Size = new System.Drawing.Size(75, 23);
+            this.makeButton.TabIndex = 0;
+            this.makeButton.Text = "button1";
+            this.makeButton.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
@@ -199,7 +199,7 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.makeButton);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -210,7 +210,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button makeButton;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button3;
